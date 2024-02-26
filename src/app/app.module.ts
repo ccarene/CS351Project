c47d1bace1cdd2ce908d910222549cc7c31370f3
TSchmitt <schm4798@kettering.edu>
2024-02-26 16:00:34 -0500

    updated dependancies and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AmplifyAngularModule, AmplifyService} from 'aws-amplify-angular';
+
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    //AmplifyAngularModule
+  ],
+  providers: [AmplifyService]
 })
 export class AppModule { }
